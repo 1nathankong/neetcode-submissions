39e9a001dd5e4f8b193ba82acf697febf0a9bdee
Nathan Kong <156246507+1nathankong@users.noreply.github.com>
2026-08-07 12:40:26 -0700

    Update: top-k-elements-in-list - submission-0

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -1,46 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        """
-        store freq values into list:
+        s = Counter(nums)
         lst = []
-        turn into dictionary: 
-        
-        example:
-        nums = [1,2,2,3,3,3], k = 2
-        {1: 1, 2: 2, 3: 3}
-
-        decrement = k
-
-        find first key with highest value:
-        which is 3. 
-        append 3: [3]
-        k = 1
-        delete 3:
-        {1: 1, 2: 2}
-
-        repeat process again:
-        2 is key with highest value
-        append 2: [3,2]
-        delete 2
-        k = 0
-
-        return list [3,2]
-        """
-        lst = []
-        if not nums or k == 0:
-            return 0
-
-        sorted_nums = sorted(nums)
-        d = defaultdict(int)
-
-        for i in sorted_nums:
-            d[i] += 1
-        print(d)
-
-        decrement = k
-        while decrement > 0:
-            max_key = max(d, key=d.get)
-            lst.append(int(max_key))
-            del d[max_key]
-            decrement -= 1
+        common = s.most_common(k)
+        for i in common:
+            lst.append(i[0])
         return lst
