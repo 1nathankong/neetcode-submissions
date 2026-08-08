f748a5a3bf39f3577a8a80990251a3136312978e
Nathan Kong <156246507+1nathankong@users.noreply.github.com>
2026-08-07 20:00:40 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for i in strs:
+            sorted_str = ''.join(sorted(i))
+            if sorted_str not in d:
+                d[sorted_str] = [i]
+            else:
+                d[sorted_str].append(i)
+        return [value for value in d.values()]       
+
+
+
+        
