d3305e48b245f7e3d0af1cde1799f89d752ba980
Nathan Kong <156246507+1nathankong@users.noreply.github.com>
2026-08-08 10:56:23 -0700

    Update: linked-list-cycle-detection - submission-0

--- Data Structures & Algorithms/linked-list-cycle-detection/submission-0.py
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.py
@@ -6,18 +6,10 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        """
-        fast and slow pointers
-
-        if fast and slow pointer equal each other return false
-        """
-
-        slow,fast = head,head
-
+        slow,fast=head,head
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
                 return True
         return False
-            
